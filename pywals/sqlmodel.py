4e1cf886866c7188d72f74564ae43d32c2f4448f
Luke Maurits <luke@maurits.id.au>
2014-04-15 15:27:23 +1200

    Added docstrings.

--- pywals/sqlmodel.py
+++ pywals/sqlmodel.py
@@ -1,4 +1,7 @@
 def create_languages_table(cursor):
+
+    """Create the "languages" table, if it doesn't already exist."""
+
     cursor.execute('''CREATE TABLE IF NOT EXISTS languages(
                 wals_code TEXT PRIMARY KEY,
                 name TEXT,
@@ -10,11 +13,17 @@ def create_languages_table(cursor):
                 iso_codes TEXT)''')
 
 def create_features_table(cursor):
+
+    """Create the "features" table, if it doesn't already exist."""
+
     cursor.execute('''CREATE TABLE IF NOT EXISTS features(
                 id TEXT PRIMARY KEY,
                 name TEXT)''')
 
 def create_values_table(cursor):
+
+    """Create the "values_" table, if it doesn't already exist."""
+
     cursor.execute('''CREATE TABLE IF NOT EXISTS values_(
                 feature_id TEXT,
                 value_id INTEGER,
@@ -22,6 +31,9 @@ def create_values_table(cursor):
                 long_desc TEXT)''')
 
 def create_data_table(cursor):
+
+    """Create the "data_points" table, if it doesn't already exist."""
+
     cursor.execute('''CREATE TABLE IF NOT EXISTS data_points(
                 wals_code TEXT,
                 feature_id TEXT,
@@ -30,12 +42,18 @@ def create_data_table(cursor):
                 FOREIGN KEY(feature_id) REFERENCES features(id))''')
 
 def create_tables(cursor):
+
+    """Create all database tables which don't already exist."""
+
     create_languages_table(cursor)
     create_features_table(cursor)
     create_values_table(cursor)
     create_data_table(cursor)
 
 def empty_tables(cursor):
+
+    """Empty all data from database tables."""
+
     cursor.execute("""DELETE FROM data_points""")
     cursor.execute("""DELETE FROM langs_per_feature_counts""")
     cursor.execute("""DELETE FROM features_per_lang_counts""")
@@ -44,6 +62,9 @@ def empty_tables(cursor):
     cursor.execute("""DELETE FROM values_""")
 
 def create_indices(cursor):
+
+    """Create various database indicies if they don't already exist."""
+
     cursor.execute("""CREATE UNIQUE INDEX IF NOT EXISTS lang_name ON languages(name)""")
     cursor.execute("""CREATE UNIQUE INDEX IF NOT EXISTS lang_family ON languages(family)""")
     cursor.execute("""CREATE INDEX IF NOT EXISTS data_wals ON data_points(wals_code)""")
